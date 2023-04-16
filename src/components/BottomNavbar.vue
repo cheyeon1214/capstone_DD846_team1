e956f56df657220213e904a71709ca9ccb87c4dd
0521kimyi <0521kimyi@gmail.com>
2023-04-17 01:18:39 +0900

    하단 navbar 컴포넌트 생성

---
+++ src/components/BottomNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-bottom-navigation v-model="value" color="teal" mode="shift">
+        <v-btn router-link to="/">
+            <v-icon>mdi-home</v-icon>
+            <span>HOME</span>
+        </v-btn>
+
+        <v-btn >
+            <v-icon>mdi-washing-machine</v-icon>
+            <span>주변세탁소</span>
+        </v-btn>
+
+        <v-btn >
+            <v-icon>mdi-format-list-bulleted</v-icon>
+            <span>이용내역</span>
+        </v-btn>
+
+        <v-btn >
+            <v-icon>mdi-account-outline</v-icon>
+            <span>MY</span>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    data: () => ({ value: null }),
+}
+</script>
