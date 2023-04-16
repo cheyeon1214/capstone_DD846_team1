1597dcf8b2dc02e1889f04891a130acb726053ad
0521kimyi <0521kimyi@gmail.com>
2023-04-16 13:20:53 +0900

    axios 전역 설정

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+// app.config.globalProperties.$axios = axios;  // [axios / http 통신] , 전역 설정
+app.provide('$axios', axios);   // axios 전역 설정 2번째 방법
+
+app.mount('#app')
