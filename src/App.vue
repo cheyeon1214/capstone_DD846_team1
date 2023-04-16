861ba2e656654e8f48bf21ac9b3847141e70ecf2
0521kimyi <0521kimyi@gmail.com>
2023-04-17 01:19:02 +0900

    Appvue에 BottomNavbar 추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <v-app>
+
+    <!-- 메인 -->
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+
+    <!-- 하단 navbar -->
+    <v-footer class="fixed-bottom">
+      <bottom-navbar />
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
+import BottomNavbar from './components/BottomNavbar.vue';
 export default {
+  components: {BottomNavbar},
   name: 'App',
+ 
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
 }
 </script>
