ad9a7d46657ff85f9d0de0923333da95055e53e9
0521kimyi <0521kimyi@gmail.com>
2023-04-16 13:21:06 +0900

    axios 전역 설정

--- src/main.js
+++ src/main.js
@@ -8,10 +8,15 @@ import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
+// createApp(App).use(router).mount('#app')
+// [앱 생성 실시]
 const app = createApp(App)
+// [axios / http 통신]
+app.config.globalProperties.$axios = axios
+// app.provide('$axios', axios);   // axios 전역 설정 2번째 방법
+// [라우터 사용 설정]
 app.use(router)
+// [vuetify 사용 설정]
 app.use(vuetify)
-// app.config.globalProperties.$axios = axios;  // [axios / http 통신] , 전역 설정
-app.provide('$axios', axios);   // axios 전역 설정 2번째 방법
-
+// [main 아이디 : 렌더링 시작점]
 app.mount('#app')
