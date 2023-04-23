26ffb7cf33c829c669ea743fe51f3b6f85a420ef
sxhxrx <shr2001@naver.com>
2023-04-23 14:47:11 +0900

    ''

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,9 @@ import ReviewListVue from '@/views/ReviewList.vue'
 import EditProfileVue from '@/views/EditProfile.vue'
 import FavoriteLaundryVue from '@/views/FavoriteLaundry.vue'
 import QnAVue from '@/views/QnA.vue'
-import NoticeVue from '@/views/Notice.vue'
+import WWNoticeVue from '@/views/WWNotice.vue'
+import NoticeDetail1Vue from '@/views/NoticeDetail1.vue'
+import NoticeDetail2Vue from '@/views/NoticeDetail2.vue'
 
 const routes = [
   {
@@ -87,9 +89,19 @@ const routes = [
     component: QnAVue
   },
   {
-    path: '/notice',
-    name: 'notice',
-    component: NoticeVue
+    path: '/wwnotice',
+    name: 'wwnotice',
+    component: WWNoticeVue
+  },
+  {
+    path: '/noticedetail1',
+    name: 'noticedetail1',
+    component: NoticeDetail1Vue
+  },
+  {
+    path: '/noticedetail2',
+    name: 'noticedetail2',
+    component: NoticeDetail2Vue
   },
 ]
 
