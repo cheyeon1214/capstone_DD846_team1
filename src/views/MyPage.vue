67a5c5541f8f3ae44c419f37bb7829172adf3fb0
sxhxrx <shr2001@naver.com>
2023-04-28 11:08:46 +0900

    레이아웃 수정

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -5,7 +5,7 @@
         <v-card class="mx-auto my-5" max-width="500" flat v-for="mp in mypage" :key="mp.id" elevation="0">
             <div class="align-end text-black" height="150">
                 <v-row>
-                    <v-col style="margin-left: 30px; margin-right: -280px; margin-top: 24px;">
+                    <v-col style="margin-left: 30px; margin-right: -45%; margin-top: 24px;">
                         <v-icon icon="mdi-account-circle" size="55"></v-icon>
                     </v-col>
                     <v-col style="margin-top: 14px;">
@@ -14,7 +14,7 @@
                             <div id="userAddr" style="margin-top: 5px;">{{ mp.userAddr }}</div>
                         </v-card-text>
                     </v-col>
-                    <v-col style="margin-top: 30px; margin-right: -230px;">
+                    <v-col style="margin-top: 30px; margin-right: -35%;">
                         <v-btn icon="mdi-wrench" color="white" size="40" to="editprofile"></v-btn>
                     </v-col>
                 </v-row>
@@ -26,7 +26,7 @@
                   <v-col cols="12">
                     <v-card color="#5E5A80" theme="dark">
                       <v-row style="margin-top: 1px ;">
-                        <v-col xs3 style="margin-left: 35px;">
+                        <v-col xs3 style="margin-left: 20px;">
                             <v-card-subtitle>이용내역</v-card-subtitle>
                             <v-card-actions>
                               <v-btn class="text-h5" id="countHistory" style="margin-left: 2px;" color="white" to="usagehistory">
@@ -34,9 +34,9 @@
                               </v-btn>
                             </v-card-actions>
                         </v-col>
-                        <v-divider vertical style="margin-left: -20px; margin-bottom: 30px; margin-right: 15px;"></v-divider>
+                        <v-divider vertical style="margin-left: -1%; margin-bottom: 30px; margin-right: 1%;"></v-divider>
                     
-                        <v-col xs3 style="margin-left: 8px;">
+                        <v-col xs3 style="">
                             <v-card-subtitle>작성리뷰</v-card-subtitle>
                             <v-card-actions>
                               <v-btn class="text-h5" id="countReview" style="margin-left: 2px;" color="white" to="reviewlist">
@@ -44,9 +44,9 @@
                               </v-btn>
                             </v-card-actions>
                         </v-col>
-                        <v-divider vertical style="margin-left: -15px; margin-bottom: 30px; margin-right: 10px;"></v-divider>
+                        <v-divider vertical style="margin-bottom: 30px; margin-left: -1%; margin-right: -2%;"></v-divider>
                     
-                        <v-col xs3 style="margin-left: 12px;">
+                        <v-col xs3 style="">
                             <v-card-subtitle style="margin-left: 10px;">Q&A</v-card-subtitle>
                             <v-card-actions>
                               <v-btn class="text-h5" id="countQnA" color="white" to="qna">{{ mp.countQnA }}</v-btn>
@@ -69,11 +69,11 @@
                         <v-row class="mx-0">
                             <v-rating :model-value=mp.stars color="amber" density="compact" 
                                 half-increments readonly size="small" style="margin-left: 15px; margin-top: 20px;"></v-rating>
-                            <div class="text-grey ms-2" style="font-size: 15px; margin-top: 23px;">{{ mp.stars }}</div>
+                            <!--<div class="text-grey ms-2" style="font-size: 15px; margin-top: 16%;">{{ mp.stars }}</div>-->
                         </v-row>
                        </div>
 
-                        <v-avatar class="ma-3" rounded="0" style="width: 230px; height: 140px;">
+                        <v-avatar class="ma-3" rounded="0" style="width: 50%; height: 140px;">
                             <v-img src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover
                                     style="margin-left: -10px;"></v-img>
                         </v-avatar>
@@ -95,15 +95,15 @@
 
                     <v-row style="margin-top: 1px;">
                       <v-col>
-                        <v-card-actions style="margin-right: -30px;">
-                            <v-btn style="width: 210px; background-color: #5E5A80; color: white; border-radius: 8px;" to="wwnotice">
+                        <v-card-actions style="margin-right: -22px;">
+                            <v-btn style="width: 95%; background-color: #5E5A80; color: white; border-radius: 8px;" to="wwnotice">
                                 공지사항
                             </v-btn>
                         </v-card-actions>
                       </v-col>
                       <v-col>
-                        <v-card-actions style="margin-left: -10px;">
-                            <v-btn variant="outlined" style="width: 210px; border-color: #5E5A80; border-radius: 8px;">
+                        <v-card-actions style="margin-left: -22px;">
+                            <v-btn variant="outlined" style="width: 95%; border-color: #5E5A80; border-radius: 8px;">
                                 로그아웃 <!--연결 필요-->
                             </v-btn>
                         </v-card-actions>
