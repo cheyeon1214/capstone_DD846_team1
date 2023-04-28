6720918335760e9b45b92625130a7583ccebcac3
sxhxrx <shr2001@naver.com>
2023-04-29 08:08:34 +0900

    완료

--- src/views/WWNotice.vue
+++ src/views/WWNotice.vue
@@ -12,7 +12,7 @@
                 </v-card-actions>
                 <div id="recordDate" style="margin-left: 17px;">2023.03.29</div>
               </v-col>
-              <v-divider class="mx-1 mb-1"></v-divider>
+              <v-divider class="mx-1 mb-1" style="margin-top: 15px;"></v-divider>
               <v-col  style="height: 80px;">
                 <v-card-actions>
                     <v-btn class="notice" style="width: 210px; font-size: medium; margin-left: -39px; margin-top: -21px;" 
@@ -23,7 +23,7 @@
                 <div id="recordDate" style="margin-left: 17px; margin-top: -20px;">2023.02.01</div>
               </v-col>
             </v-row>
-            <v-divider class="mx-1 mb-1"></v-divider>
+            <v-divider class="mx-1 mb-1" style="margin-top: 10px;"></v-divider>
         </v-card>
     </div>
 </template>
