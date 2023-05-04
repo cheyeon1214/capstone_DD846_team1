3b43cd91e7ee45bf730f697b1b12d37655d56a8a
sxhxrx <shr2001@naver.com>
2023-05-05 02:56:23 +0900

    버튼 구현중

--- src/views/ManageOrder.vue
+++ src/views/ManageOrder.vue
@@ -2,37 +2,40 @@
 
 <template>
     <div class="manageorder">
+        <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
+            <b>&nbsp;&nbsp;세탁물 요청</b>
+        </div>
+        <!--<v-divider class="mx-1 mb-1" style="margin-top: 3%;"></v-divider>-->
         <v-card class="mx-auto my-5" max-width="500" v-for="(request, index) in requests" v-bind:key="request.id" elevation="0">
-            <div v-if="index === 0 || request.date !== requests[index - 1].date" style="margin-bottom: 10px;">
-                <div id="date">
+            <div v-if="index === 0 || request.date !== requests[index - 1].date" style="margin-bottom: 5px;">
+                <div id="date" style="margin-left: 10px; font-size: 14px;">
                     <b>{{ request.date }}</b>
                 </div>
             </div>
-            <v-card color="#5E5A80" style="color: white;"> <!--variant="outlined"-->
-                <v-row>
-                <v-col><v-card-text id="userId" style="margin-top: 4px; font-size: 15px;">
-                    <b>{{ request.userId }}&nbsp;&nbsp;|</b>
-                </v-card-text></v-col>
-                <v-col style=""><v-card-text id="name" style="font-size: 15px; margin-left: -60px;">
-                    <b>&nbsp;&nbsp;{{ request.name }}</b>
-                </v-card-text></v-col>
-                <v-col><v-card-text id="requirement" style="margin-top: 5px; margin-left: -100px;">
-                    &nbsp;&nbsp;&nbsp;{{ request.requirement }}
-                </v-card-text></v-col>
-                <v-card-actions style="margin-right: -7px;">
-                    <v-btn id="accept" icon="mdi-check" variant="outlined" size="35px" style="border-radius: 15%;"></v-btn>
-                </v-card-actions>
-                <v-card-actions style="margin-right: -7px;">
-                    <v-btn id="cancel" icon="mdi-close" variant="outlined" size="35px" style="border-radius: 15%;"></v-btn>
-                </v-card-actions>
-                <v-card-actions style="margin-right: 20px;">
-                    <v-btn id="seeMore" icon="mdi-plus" variant="outlined" size="35px" style="border-radius: 15%;"></v-btn>
-                </v-card-actions>
-              </v-row>
+            <v-card color="#5E5A80" style="color: white;">
+                <v-row style="margin-top: -19px; margin-bottom: -18px;">
+                    <v-col><v-card-text id="userId" style="margin-top: 4px; font-size: 14px;">
+                        <b>{{ request.userId }}&nbsp;&nbsp;|</b>
+                    </v-card-text></v-col>
+                    <v-col><v-card-text id="name" style="font-size: 14px; margin-left: -60px;">
+                        <b>&nbsp;&nbsp;&nbsp;{{ request.name }}</b>
+                    </v-card-text></v-col>
+                    <v-col style="margin-top: 3px;"><v-card-text id="requirement" style="margin-top: 5px; margin-left: -190px;">
+                        &nbsp;<p>{{ request.requirement }}</p>
+                    </v-card-text></v-col>
+                    <v-card-actions style="margin-right: -10px;">
+                      <v-btn id="accept" icon="mdi-check" variant="outlined" size="32px" style="border-radius: 15%;"></v-btn>
+                    </v-card-actions>
+                    <v-card-actions style="margin-right: -10px;">
+                      <v-btn id="cancel" icon="mdi-close" variant="outlined" size="32px" style="border-radius: 15%;" @click="rejectRequest(index)"></v-btn>
+                    </v-card-actions>
+                    <v-card-actions style="margin-right: 17px;">
+                      <v-btn id="moreInfo" icon="mdi-plus" variant="outlined" size="32px" style="border-radius: 15%;" to="orderdetails"></v-btn>
+                    </v-card-actions>
+                </v-row>
             </v-card>
         </v-card>
-        
-        <v-divider class="mx-1 mb-1"></v-divider>
+        <v-divider class="mx-1 mb-1" style="margin-top: 30px;"></v-divider>
     </div>
 </template>
 
@@ -51,6 +54,21 @@ export default {
         } catch (e) {
             console.error(e);
         }
-    }
+    },
+    methods: {
+        async rejectRequest(index) {
+            try {
+                const requestId = this.requests[index].id;
+                await axios.delete(`http://localhost:3012/requests/${requestId}`);
+                this.requests.splice(index, 1);
+                this.showAlert("세탁 요청이 거절되었습니다.");
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        showAlert(message) {
+            alert(message);
+        },
+    },
 }
 </script>
