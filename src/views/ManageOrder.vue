3d98470900c10403f9d194c1c3ec911d873332aa
sxhxrx <shr2001@naver.com>
2023-05-08 09:39:27 +0900

    수락버튼 수정중

--- src/views/ManageOrder.vue
+++ src/views/ManageOrder.vue
@@ -6,7 +6,7 @@
             <b>&nbsp;&nbsp;세탁물 요청</b>
         </div>
         <!--<v-divider class="mx-1 mb-1" style="margin-top: 3%;"></v-divider>-->
-        <v-card class="mx-auto my-5" max-width="500" v-for="(request, index) in requests" v-bind:key="request.id" elevation="0">
+        <v-card class="mx-auto my-5" max-width="500" v-for="(request, index) in filteredRequests('요청중')" v-bind:key="request.id" elevation="0">
             <div v-if="index === 0 || request.date !== requests[index - 1].date" style="margin-bottom: 5px;">
                 <div id="date" style="margin-left: 10px; font-size: 14px;">
                     <b>{{ request.date }}</b>
@@ -40,7 +40,7 @@
 
         <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
             <div id="beforeShipping" style="margin-bottom: 10px;"><b>&nbsp;&nbsp;배송전</b></div>
-            <v-card v-for="(request, index) in beforeShipping" :key="request.id" elevation="0">
+            <v-card v-for="(request, index) in filteredRequests('배송전')" :key="request.id" elevation="0">
                 <v-card color="#5E5A80" style="color: white; margin-bottom: 10px;">
                     <v-row style="margin-top: -19px; margin-bottom: -18px;">
                         <v-col><v-card-text id="userId" style="margin-top: 4px; font-size: 14px;">
@@ -90,25 +90,41 @@ export default {
                 console.error(e);
             }
         },
+        filteredRequests(status) {  //배송 상태별로 구분
+            return this.requests.filter(request => request.status === status);
+        },
+
+
+
         // 수락 버튼 -> 배송전으로 이동
         async clickAccept(index) {
             try {
-                const requestId = this.requests[index].id;
-                const request = this.requests[index];
+                const requestId = this.$route.query.id;
+                axios.get(`http://localhost:3012/requests/${requestId}`)
+                //const requestId = this.requests[index].id;
+                //const request = this.requests[index];
                 request.status = "배송전";    // JSON 데이터의 "status" 값을 "배송전"으로 수정
                 await axios.put(`http://localhost:3012/requests/${requestId}`, request);
+                
+
                 this.showAlert("세탁 요청이 수락되었습니다.");
-                // 해당 카드를 beforeShipping 배열로 이동
                 const acceptedRequest = this.requests.splice(index, 1)[0];
                 this.beforeShipping.push(acceptedRequest);
+
+                /* 데이터가 일치하는 경우 배송전 이동
+                if (request.status === "배송전") {
+                    // 해당 요청을 "beforeShipping" 섹션으로 이동
+                    const acceptedRequest = this.requests.splice(index, 1)[0];
+                    this.beforeShipping.push(acceptedRequest);
+                }*/
             } catch (e) {
                 console.error(e);
             }
         },
-        //배송시작 버튼 -> 배송중으로 이동
+        /*배송시작 버튼 -> 배송중으로 이동
         moveToIng(index) {
 
-        },
+        },*/
 
         showAlert(message) {
             alert(message);
