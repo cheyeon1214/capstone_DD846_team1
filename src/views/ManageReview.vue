c2efe2dfe9fc360ab7e8704a2c2cc43c087053f5
sxhxrx <shr2001@naver.com>
2023-05-09 20:43:49 +0900

    리뷰 관리 수정중

--- src/views/ManageReview.vue
+++ src/views/ManageReview.vue
@@ -4,7 +4,7 @@
 <template>
   <v-card class="mx-auto my-5" max-width="400" elevation="0"><br>
     <div style="margin-top: -28px;"></div>
-      <v-card  v-for="(r, i) in reviews" :key="r.id" elevation="0"  style="margin-bottom: 10px;">
+      <v-card  v-for="(r) in reviews" :key="r.id" elevation="0"  style="margin-bottom: 10px;">
           <v-card-text style="margin-bottom: -5px;">
               <span id="userID" style="font-weight: bold; font-size: 15px;">
                   {{ r.userId }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -26,21 +26,13 @@
             </v-col>
             <v-col cols="9" style="margin-left: 6px; margin-right: -25px;">
               <!--<v-text-field v-model="reply[i]" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field>-->
-              <v-text-field v-model="newReply" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field> <!--답글 출력-->
+              <v-text-field v-model="replyValue" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field>
             </v-col>
             <v-col cols="3" style="margin-right: -30px;">
               <v-card-actions>
-                  <v-btn icon="mdi-check" style="color: #0C70FE;" @click="sendReply(i)"></v-btn>
+                  <v-btn icon="mdi-check" style="color: #0C70FE;" @click="sendReply(r.id)"></v-btn>
               </v-card-actions>
             </v-col>
-
-            <!--답글 테스트-->
-            <v-card style="margin-left: 15px; margin-bottom: 20px; margin-top: -10px;">
-              <v-card-text>
-                ⮡ &nbsp;답글: {{reply[i]}}
-              </v-card-text>
-            </v-card>
-
           </v-row>
           <v-divider class="mx-1 mb-1"></v-divider>
       </v-card>
@@ -54,23 +46,48 @@ export default {
 data: () => ({
   show: false,
   reviews: [],
-  reply: [], // 답글 저장
-  newReply: "", // 답글 데이터 출력 위한 변수
+  reply: [],
+  replyValue: "",
 }),
 async created() {
   try {
     const res = await axios.get("http://localhost:3006/reviews");
     this.reviews = res.data;
 
-    const response = await axios.get("http://localhost:3022/replies");
-    this.reply = response.data;
+    //const response = await axios.get("http://localhost:3022/replies");
+    //this.reply = response.data;
 
   } catch (e) {
     console.error(e);
   }
 },
 methods: {
-  async sendReply(i) {
+  async sendReply(reviewId) {
+    try {
+      const response = await axios.get(`http://localhost:3006/reviews/${reviewId}`);
+      const review = response.data;
+      review.reply = this.replyValue;   // 텍스트 필드에서 입력받은 값
+      await axios.put(`http://localhost:3006/reviews/${reviewId}`, review);
+      this.showAlert("답글이 등록되었습니다.");
+      
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  showAlert(message) {
+            alert(message);
+        },
+},
+headers: {
+  'Access-Control-Allow-Origin': '*'
+}
+};
+
+
+
+
+/*
+async sendReply(i) {
     try {
       const response = await axios.post("http://localhost:3022/replies", {
       reply: this.reply[i], //this.replay[i]
@@ -86,9 +103,6 @@ methods: {
       console.error(e);
     }
   },
-},
-headers: {
-  'Access-Control-Allow-Origin': '*'
-}
-};
+*/
+
 </script>
