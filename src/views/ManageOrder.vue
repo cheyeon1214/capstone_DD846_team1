59fed6c12518541354a11d5c0bacafb43ecbbc04
sxhxrx <shr2001@naver.com>
2023-05-09 10:56:16 +0900

    각 버튼 구현 완료

--- src/views/ManageOrder.vue
+++ src/views/ManageOrder.vue
@@ -5,8 +5,7 @@
         <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
             <b>&nbsp;&nbsp;세탁물 요청</b>
         </div>
-        <!--<v-divider class="mx-1 mb-1" style="margin-top: 3%;"></v-divider>-->
-        <v-card class="mx-auto my-5" max-width="500" v-for="(request, index) in filteredRequests('요청중')" v-bind:key="request.id" elevation="0">
+        <v-card class="mx-auto my-5" max-width="500" v-for="(request, index) in filteredRequests('요청중')" v-bind:key="request.id" elevation="0" query="request.id">
             <div v-if="index === 0 || request.date !== requests[index - 1].date" style="margin-bottom: 5px;">
                 <div id="date" style="margin-left: 10px; font-size: 14px;">
                     <b>{{ request.date }}</b>
@@ -24,7 +23,7 @@
                         &nbsp;<p>{{ request.requirement }}</p>
                     </v-card-text></v-col>
                     <v-card-actions style="margin-right: -10px;">
-                      <v-btn id="accept" icon="mdi-check" variant="outlined" size="32px" style="border-radius: 15%;" @click="clickAccept(index)"></v-btn>
+                      <v-btn id="accept" icon="mdi-check" variant="outlined" size="32px" style="border-radius: 15%;" @click="clickAccept(request.id)"></v-btn>
                     </v-card-actions>
                     <v-card-actions style="margin-right: -10px;">
                       <v-btn id="cancel" icon="mdi-close" variant="outlined" size="32px" style="border-radius: 15%;" @click="rejectRequest(index)"></v-btn>
@@ -39,7 +38,7 @@
         <v-divider class="mx-1 mb-1" style="margin-top: 30px;"></v-divider>
 
         <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
-            <div id="beforeShipping" style="margin-bottom: 10px;"><b>&nbsp;&nbsp;배송전</b></div>
+            <div id="beforeDelivery" style="margin-bottom: 10px;"><b>&nbsp;&nbsp;배송전</b></div>
             <v-card v-for="(request, index) in filteredRequests('배송전')" :key="request.id" elevation="0">
                 <v-card color="#5E5A80" style="color: white; margin-bottom: 10px;">
                     <v-row style="margin-top: -19px; margin-bottom: -18px;">
@@ -53,7 +52,50 @@
                             &nbsp;<p>{{ request.requirement }}</p>
                         </v-card-text></v-col>
                         <v-card-actions style="margin-right: 20px;">
-                          <v-btn id="moveIng" variant="outlined" style="border-radius: 20px; font-size: 12px;" @click="moveToIng(index)">배송시작</v-btn>
+                          <v-btn id="start" variant="outlined" style="border-radius: 20px; font-size: 12px;" @click="clickStartDelivery(request.id)">배송시작</v-btn>
+                        </v-card-actions>
+                    </v-row>
+                </v-card>
+            </v-card>
+        </div><v-divider class="mx-1 mb-1" style="margin-top: 30px;"></v-divider>
+
+        <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
+            <div id="onDelivery" style="margin-bottom: 10px;"><b>&nbsp;&nbsp;배송중</b></div>
+            <v-card v-for="(request, index) in filteredRequests('배송중')" :key="request.id" elevation="0">
+                <v-card color="#5E5A80" style="color: white; margin-bottom: 10px;">
+                    <v-row style="margin-top: -19px; margin-bottom: -18px;">
+                        <v-col><v-card-text id="userId" style="margin-top: 4px; font-size: 14px;">
+                            <b>{{ request.userId }}&nbsp;&nbsp;|</b>
+                        </v-card-text></v-col>
+                        <v-col><v-card-text id="name" style="font-size: 14px; margin-left: -60px;">
+                            <b>&nbsp;&nbsp;&nbsp;{{ request.name }}</b>
+                        </v-card-text></v-col>
+                        <v-col style="margin-top: 3px;"><v-card-text id="requirement" style="margin-top: 5px; margin-left: -190px;">
+                            &nbsp;<p>{{ request.requirement }}</p>
+                        </v-card-text></v-col>
+                        <v-card-actions style="margin-right: 20px;">
+                          <v-btn id="complete" variant="outlined" style="border-radius: 20px; font-size: 12px;" @click="clickCompleteDelivery(request.id)">배송완료</v-btn>
+                        </v-card-actions>
+                    </v-row>
+                </v-card>
+            </v-card>
+        </div><v-divider class="mx-1 mb-1" style="margin-top: 30px;"></v-divider>
+
+        <div class="mx-auto my-5" style="margin-top: 15px; max-width: 500px;">
+            <div id="completeDelivery" style="margin-bottom: 10px;"><b>&nbsp;&nbsp;배송완료</b></div>
+            <v-card v-for="(request, index) in filteredRequests('배송완료')" :key="request.id" elevation="0">
+                <v-card color="#5E5A80" style="color: white; margin-bottom: 10px;">
+                    <v-row style="margin-top: -19px; margin-bottom: -18px;">
+                        <v-col><v-card-text id="userId" style="margin-top: 4px; font-size: 14px;">
+                            <b>{{ request.userId }}&nbsp;&nbsp;|</b>
+                        </v-card-text></v-col>
+                        <v-col><v-card-text id="name" style="font-size: 14px; margin-left: -60px;">
+                            <b>&nbsp;&nbsp;&nbsp;{{ request.name }}</b>
+                        </v-card-text></v-col>
+                        <v-col style="margin-top: 3px;"><v-card-text id="requirement" style="margin-top: 5px; margin-left: -190px;">
+                            &nbsp;<p>{{ request.requirement }}</p>
+                        </v-card-text></v-col>
+                        <v-card-actions style="margin-right: 20px;">
                         </v-card-actions>
                     </v-row>
                 </v-card>
@@ -93,39 +135,45 @@ export default {
         filteredRequests(status) {  //배송 상태별로 구분
             return this.requests.filter(request => request.status === status);
         },
-
-
-
         // 수락 버튼 -> 배송전으로 이동
-        async clickAccept(index) {
+        async clickAccept(requestId) {
             try {
-                const requestId = this.$route.query.id;
-                axios.get(`http://localhost:3012/requests/${requestId}`)
-                //const requestId = this.requests[index].id;
-                //const request = this.requests[index];
-                request.status = "배송전";    // JSON 데이터의 "status" 값을 "배송전"으로 수정
+                const response = await axios.get(`http://localhost:3012/requests/${requestId}`);
+                const request = response.data;
+                request.status = "배송전";  // JSON 데이터의 "status" 값을 "배송전"으로 수정
                 await axios.put(`http://localhost:3012/requests/${requestId}`, request);
-                
-
+                window.location.reload(); // 페이지 새로고침 (= this.$router.go();)   
                 this.showAlert("세탁 요청이 수락되었습니다.");
-                const acceptedRequest = this.requests.splice(index, 1)[0];
-                this.beforeShipping.push(acceptedRequest);
-
-                /* 데이터가 일치하는 경우 배송전 이동
-                if (request.status === "배송전") {
-                    // 해당 요청을 "beforeShipping" 섹션으로 이동
-                    const acceptedRequest = this.requests.splice(index, 1)[0];
-                    this.beforeShipping.push(acceptedRequest);
-                }*/
             } catch (e) {
                 console.error(e);
             }
         },
-        /*배송시작 버튼 -> 배송중으로 이동
-        moveToIng(index) {
-
-        },*/
-
+        // 배송시작 버튼 -> 배송중으로 이동
+        async clickStartDelivery(requestId) {
+            try {
+                const response = await axios.get(`http://localhost:3012/requests/${requestId}`);
+                const request = response.data;
+                request.status = "배송중";
+                await axios.put(`http://localhost:3012/requests/${requestId}`, request);
+                window.location.reload();
+                this.showAlert("배송이 시작되었습니다.");
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        // 배송완료 버튼 -> 배송완료로 이동
+        async clickCompleteDelivery(requestId) {
+            try {
+                const response = await axios.get(`http://localhost:3012/requests/${requestId}`);
+                const request = response.data;
+                request.status = "배송완료";
+                await axios.put(`http://localhost:3012/requests/${requestId}`, request);
+                window.location.reload(); 
+                this.showAlert("배송이 완료되었습니다.");
+            } catch (e) {
+                console.error(e);
+            }
+        },
         showAlert(message) {
             alert(message);
         },
