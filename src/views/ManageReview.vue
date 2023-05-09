9e656864d06edfe44e23186a0e5652a3300f9e61
sxhxrx <shr2001@naver.com>
2023-05-10 03:00:18 +0900

    리뷰 관리

--- src/views/ManageReview.vue
+++ src/views/ManageReview.vue
@@ -4,7 +4,7 @@
 <template>
   <v-card class="mx-auto my-5" max-width="400" elevation="0"><br>
     <div style="margin-top: -28px;"></div>
-      <v-card  v-for="(r) in reviews" :key="r.id" elevation="0"  style="margin-bottom: 10px;">
+      <v-card  v-for="r in reviews" :key="r.id" elevation="0" style="margin-bottom: 10px;">
           <v-card-text style="margin-bottom: -5px;">
               <span id="userID" style="font-weight: bold; font-size: 15px;">
                   {{ r.userId }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -25,8 +25,7 @@
               <v-card-text style="font-size: larger;">⮡ </v-card-text>
             </v-col>
             <v-col cols="9" style="margin-left: 6px; margin-right: -25px;">
-              <!--<v-text-field v-model="reply[i]" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field>-->
-              <v-text-field v-model="replyValue" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field>
+              <v-text-field v-model="r.replyValue" placeholder="답글을 남겨주세요" variant="outlined"></v-text-field>
             </v-col>
             <v-col cols="3" style="margin-right: -30px;">
               <v-card-actions>
@@ -53,10 +52,6 @@ async created() {
   try {
     const res = await axios.get("http://localhost:3006/reviews");
     this.reviews = res.data;
-
-    //const response = await axios.get("http://localhost:3022/replies");
-    //this.reply = response.data;
-
   } catch (e) {
     console.error(e);
   }
@@ -64,11 +59,11 @@ async created() {
 methods: { 
   async sendReply(reviewId) {
     try {
-      const response = await axios.get(`http://localhost:3006/reviews/${reviewId}`);
-      const review = response.data;
+      const review = this.reviews.find((r) => r.id === reviewId);
       review.reply = this.replyValue;   // 텍스트 필드에서 입력받은 값
       await axios.put(`http://localhost:3006/reviews/${reviewId}`, review);
       this.showAlert("답글이 등록되었습니다.");
+      review.replyValue = '';   // 텍스트 필드 비우기
       
     } catch (e) {
       console.error(e);
@@ -82,27 +77,4 @@ headers: {
   'Access-Control-Allow-Origin': '*'
 }
 };
-
-
-
-
-/*
-async sendReply(i) {
-    try {
-      const response = await axios.post("http://localhost:3022/replies", {
-      reply: this.reply[i], //this.replay[i]
-      });
-      console.log(response.data); // POST 요청에 대한 응답
-      this.reply[i] = new Array(this.reviews.length).fill(""); // 리뷰 아이템 개수만큼 빈 문자열로 입력 필드 채움
-
-      //답글 데이터 출력
-      this.reply[i] = this.newReply; // 입력된 값을 reply 배열에 업데이트
-      this.newReply = ""; // 텍스트 필드 초기화 (답글 출력)
-      
-    } catch (e) {
-      console.error(e);
-    }
-  },
-*/
-
 </script>
