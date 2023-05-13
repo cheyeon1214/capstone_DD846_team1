927ee4db47be3d7c583df21dd1bfd9052fb95102
sxhxrx <shr2001@naver.com>
2023-05-13 19:29:44 +0900

    홈

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,40 +2,14 @@
 <!--서혜린-->
 
 <template>
-  <!-- <HelloWorld /> -->
-  <!-- 상단 슬라이드 -->
   <v-carousel cycle height="250" hide-delimiter-background show-arrows="hover">
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet :color="colors[i]" height="100%">
-        <div class="d-flex fill-height justify-center align-center">
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </div>
+    <v-carousel-item v-for="(slide) in slides">
+      <v-sheet height="100%">
+        <v-img :src="slide.imageSrc" :alt="slide.altText" height="100%" contain></v-img>
       </v-sheet>
     </v-carousel-item>
   </v-carousel>
 
-  <!-- 메인 컨텐츠 -->
-   <!-- <v-container class="d-flex align-center justify-center">
-      <v-row
-        :justify="isMobile ? 'center' : 'none'"
-        :align="isMobile ? 'none' : 'center'"
-      >
-        <v-col
-          v-for="n in 3"
-          :key="n"
-          cols="12"
-          md="4"
-          class="d-flex align-center"
-        >
-          <v-card class="mx-auto" width="300" prepend-icon="mdi-home">
-            <template v-slot:title>This is a title</template>
-            <v-card-text>This is content</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container> -->
     <v-card to="managelaundry"
       class="mx-auto my-10"
       width="300"
@@ -80,36 +54,31 @@
 </template>
 
 <script>
-// import { defineComponent } from 'vue';
-
-// // Components
-// import HelloWorld from '../components/HelloWorld.vue';
-
-// export default defineComponent({
-//   name: 'HomeView',
-
-//   components: {
-//     HelloWorld,
-//   },
-// });
 export default {
   data() {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
-      ],
       slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth',
+        {
+          imageSrc: require('@/assets/images/001.jpg'),
+          altText: 'First Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/002.jpg'),
+          altText: 'Second Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/003.jpg'),
+          altText: 'Third Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/004.jpg'),
+          altText: 'Fourth Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/005.jpg'),
+          altText: 'Fifth Slide',
+        },
       ],
-      // isMobile: false,
     }
   },
 }
