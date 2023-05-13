bea7b2b72eea43ebbbc73a2ea0523fdbf003bd5d
sxhxrx <shr2001@naver.com>
2023-05-14 01:34:39 +0900

    홈

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,13 @@
 
 <template>
   <v-carousel cycle height="250" hide-delimiter-background show-arrows="hover">
-    <v-carousel-item v-for="(slide) in slides">
+    <v-carousel-item v-for="slide in slides" :key="slide.id">
       <v-sheet height="100%">
         <v-img :src="slide.imageSrc" :alt="slide.altText" height="100%" contain></v-img>
       </v-sheet>
     </v-carousel-item>
   </v-carousel>
-  
+
 </template>
 
 <script>
